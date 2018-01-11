fc7a35273e9a2d71abced85ed72bd49c0aaee1d6
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-01-11 15:25:58 -0600

    spread operator examples

--- app/index.js
+++ app/index.js
@@ -3,8 +3,14 @@ const arrNum = [1,2,3];
 arrNum.push(4,5);
 console.log(arrNum);
 
-
+//general syntax
 let a ="hello";
 let b ="world";
 let c =`Hello ES6 & ${a} ${b} today!`;
 console.log(c);
+
+//using the spread operator
+let arrA = [7,8,9];
+let arrB = [6, ...arrA,10];
+console.log(arrB);
+
