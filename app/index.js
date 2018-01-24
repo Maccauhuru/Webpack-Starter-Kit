ec212aeb2b7be1cea18c7780e968fd752c0dbb2d
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-01-23 22:11:49 -0600

    changed spread parameters

--- app/index.js
+++ app/index.js
@@ -14,3 +14,12 @@ let arrA = [7,8,9];
 let arrB = [6, ...arrA,10];
 console.log(arrB);
 
+//using the spread function 2
+
+function printItems(a,b,c){
+    console.log(a,b,c);
+}
+
+let pnums = [10,20,30];
+//printItems(pnums[0], pnums[1], pnums[2]);
+printItems(...pnums);
